75331f79b3ebc1de890617e80846e2d1ee677482
Augusto Gomez <agomezsantos95@gmail.com>
2023-11-28 18:58:39 +0100

    Add Routes to App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,19 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import Container from "../../styles/Container/Container";
 import Header from "../Header/Header";
+import ListPage from "../../pages/ListPage/ListPage";
 
 const App = () => (
   <>
     <Header />
-    <Container>
-      <main className="main-content"></main>
-    </Container>
+    <main className="main-content">
+      <Container>
+        <Routes>
+          <Route path="/" element={<Navigate to={"/home"} />} />
+          <Route path="/home" element={<ListPage />} />
+        </Routes>
+      </Container>
+    </main>
   </>
 );
 
