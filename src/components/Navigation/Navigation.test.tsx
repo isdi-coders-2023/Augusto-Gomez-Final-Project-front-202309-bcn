17aa4ed9d2714d9703aa774b3b0b928994b083c1
Augusto Gomez <agomezsantos95@gmail.com>
2023-11-29 18:14:18 +0100

    Refactor tests to receive context and mainTheme from customRender

--- src/components/Navigation/Navigation.test.tsx
+++ src/components/Navigation/Navigation.test.tsx
@@ -1,19 +1,16 @@
-import { ThemeProvider } from "styled-components";
 import Navigation from "./Navigation";
-import { render, screen } from "@testing-library/react";
-import mainTheme from "../../styles/mainTheme";
+import { screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
+import customRender from "../../testUtils/testUtils";
 
 describe("Given a Navigation component", () => {
   describe("When it is rendered", () => {
     test("Then it should show an image with an alternative text that says 'House navigation icon'", () => {
       const expectedAlternativeText = "House navigation icon";
 
-      render(
+      customRender(
         <BrowserRouter>
-          <ThemeProvider theme={mainTheme}>
-            <Navigation />
-          </ThemeProvider>
+          <Navigation />
         </BrowserRouter>,
       );
 
