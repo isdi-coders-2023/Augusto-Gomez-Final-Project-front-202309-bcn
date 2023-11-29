a9af2594c84d0a14915c63cd13d764122deb5ea8
Augusto Gomez <agomezsantos95@gmail.com>
2023-11-29 13:06:59 +0100

    Add styles to MovieList component

---
+++ src/components/MovieList/MovieListStyled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const MovieListStyled = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 80px;
+`;
+
+export default MovieListStyled;
