22d06ecef9a20ec483e4e704164e5c4987d41e47
Augusto Gomez <agomezsantos95@gmail.com>
2023-11-29 13:36:51 +0100

    Link MovieList to its style component

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { loadMoviesActionCreator } from "../../store/features/movies/moviesSlice";
 import { moviesMock } from "../../mocks/movieMocks";
+import MovieListStyled from "./MovieListStyled";
 
 const MovieList = () => {
   const moviesState = useAppSelector((state) => state.movieState);
@@ -13,11 +14,11 @@ const MovieList = () => {
   }, [dispatch]);
 
   return (
-    <ul className="movie-list">
+    <MovieListStyled className="movie-list">
       {moviesState.movies.map((movie) => (
         <h2 className="movie-list__item">{movie.name}</h2>
       ))}
-    </ul>
+    </MovieListStyled>
   );
 };
 
