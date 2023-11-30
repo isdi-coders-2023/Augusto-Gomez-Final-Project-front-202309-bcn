7c37fc1592105909b4c39b1b26445d061b593d74
Augusto Gomez <agomezsantos95@gmail.com>
2023-11-30 17:11:54 +0100

    Add testing suites to App

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -3,7 +3,7 @@ import { MemoryRouter } from "react-router-dom";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
 import mainTheme from "../../styles/mainTheme";
-import customRender from "../../testUtils/testUtils";
+import { customRender } from "../../testUtils/testUtils";
 
 describe("Given an App component", () => {
   describe("When it is rendered on screen on the HomePage", () => {
@@ -21,6 +21,38 @@ describe("Given an App component", () => {
       const title = screen.getByRole("heading", { name: expectedTitle });
 
       expect(title).toBeInTheDocument();
+    });
+
+    test("Then you should see an 'Arrival' text in a heading on a card", () => {
+      const expectedTitle = "Arrival";
+
+      customRender(
+        <MemoryRouter>
+          <ThemeProvider theme={mainTheme}>
+            <App />
+          </ThemeProvider>
+        </MemoryRouter>,
+      );
+
+      const title = screen.getByRole("heading", { name: expectedTitle });
+
+      expect(title).toBeInTheDocument();
+    });
+
+    test("Then you should see a 'Cover of La La Land' alternative text on an image in a card", () => {
+      const expectedAlternativeText = "Cover of La La Land";
+
+      customRender(
+        <MemoryRouter>
+          <ThemeProvider theme={mainTheme}>
+            <App />
+          </ThemeProvider>
+        </MemoryRouter>,
+      );
+
+      const image = screen.getByAltText(expectedAlternativeText);
+
+      expect(image).toBeInTheDocument();
     });
   });
 });
