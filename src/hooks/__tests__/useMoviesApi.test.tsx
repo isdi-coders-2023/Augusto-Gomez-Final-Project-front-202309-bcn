7e1a90a21b1f245ed82f16afe53def9e21bff0f5
Augusto Gomez <agomezsantos95@gmail.com>
2023-12-04 17:50:27 +0100

    Add testing suite for getMovies sad path

--- src/hooks/__tests__/useMoviesApi.test.tsx
+++ src/hooks/__tests__/useMoviesApi.test.tsx
@@ -1,7 +1,11 @@
-import { renderHook } from "@testing-library/react";
+import { renderHook, screen, waitFor } from "@testing-library/react";
 import useMoviesApi from "../useMoviesApi";
-import { providerWrapper } from "../../testUtils/testUtils";
+import { customRender, providerWrapper } from "../../testUtils/testUtils";
 import { moviesMock } from "../../mocks/moviesMocks";
+import App from "../../components/App/App";
+import { server } from "../../mocks/msw/node";
+import { errorHandlers } from "../../mocks/msw/errorHandlers";
+import { MemoryRouter } from "react-router-dom";
 
 describe("Given a useMoviesApi custom hook", () => {
   describe("When it calls its getMovies function", () => {
@@ -15,6 +19,32 @@ describe("Given a useMoviesApi custom hook", () => {
       const expectedMovies = await getMovies(import.meta.env.VITE_API_URL);
 
       expect(expectedMovies).toStrictEqual(moviesMock);
+    });
+  });
+
+  describe("When it calls its getMovies function and it throws an error", () => {
+    test("Then it should navigate to Error page", async () => {
+      server.use(...errorHandlers);
+
+      customRender(
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>,
+      );
+
+      const {
+        result: {
+          current: { getMovies },
+        },
+      } = renderHook(() => useMoviesApi(), { wrapper: providerWrapper });
+
+      await getMovies(import.meta.env.VITE_API_URL);
+
+      await waitFor(() => {
+        const image = screen.getByAltText("Cut cinema reel drawing");
+
+        expect(image).toBeInTheDocument();
+      });
     });
   });
 });
