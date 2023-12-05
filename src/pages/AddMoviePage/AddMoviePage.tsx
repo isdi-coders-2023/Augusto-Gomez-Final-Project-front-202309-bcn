b4494b617f6fea2ccbadfd256913b61549b1cbea
Augusto Gomez <agomezsantos95@gmail.com>
2023-12-05 15:36:44 +0100

    Add styles to AddMoviePage

--- src/pages/AddMoviePage/AddMoviePage.tsx
+++ src/pages/AddMoviePage/AddMoviePage.tsx
@@ -1,7 +1,14 @@
+import Form from "../../components/Form/Form";
+import TitleStyled from "../../styles/shared/TitleStyled";
+import AddMoviePageStyled from "./AddMoviePageStyled";
+
 const AddMoviePage = (): React.ReactElement => (
-  <section className="add-movie-page">
-    <h2 className="add-movie-page__title">Add your own movie</h2>
-  </section>
+  <AddMoviePageStyled className="add-movie-page">
+    <TitleStyled className="add-movie-page__title">
+      Add your own movie
+    </TitleStyled>
+    <Form />
+  </AddMoviePageStyled>
 );
 
 export default AddMoviePage;
---
+++ src/pages/AddMoviePage/AddMoviePageStyled.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+const AddMoviePageStyled = styled.section``;
+
+export default AddMoviePageStyled;
