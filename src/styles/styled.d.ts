85a535aa66616ecbdd261cbc6a6b62cb9f4bc1de
Augusto Gomez <agomezsantos95@gmail.com>
2023-12-06 17:43:14 +0100

    Change mainTheme properties to add background colors

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -4,6 +4,8 @@ declare module "styled-components" {
   export interface DefaultTheme {
     colors: {
       background: string;
+      formBackground: string;
+      buttonBackground: string;
       light: string;
       dark: string;
       modify: string;
