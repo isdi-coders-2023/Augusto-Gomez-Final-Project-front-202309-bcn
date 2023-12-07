0f9554530a6138b250bf5ef17151ad644daef91c
Augusto Gomez <agomezsantos95@gmail.com>
2023-12-07 19:47:34 +0100

    Add Form integration test to App component

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,4 +1,4 @@
-import { screen } from "@testing-library/react";
+import { fireEvent, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
@@ -79,6 +79,57 @@ describe("Given an App component", () => {
       });
 
       expect(addMoviePageHeading).toBeInTheDocument();
+    });
+  });
+
+  describe("When it is rendered on the AddPage and you fill the form fields and click the submit button", () => {
+    test("Then you should navigate to the ListPage and see a 'Our movies' title on a heading", async () => {
+      const buttonText = "Add";
+      const listPageHeadingText = "Our movies";
+      const testText = "https://prueba.com";
+      const fieldNames = [
+        "Name",
+        "Director",
+        "Writer",
+        "Stars",
+        "Genre",
+        "Image Url",
+        "Description",
+      ];
+      const releaseDateLabel = "Release date";
+
+      customRender(
+        <MemoryRouter initialEntries={["/add-movie"]}>
+          <App />
+        </MemoryRouter>,
+      );
+
+      const addButton = screen.getByRole("button", { name: buttonText });
+
+      const releaseDateField = screen.getByLabelText(releaseDateLabel);
+
+      const scoreField = screen.getByRole("slider", { name: "Score" });
+
+      for (const text of fieldNames) {
+        await userEvent.type(
+          screen.getByRole("textbox", { name: text }),
+          testText,
+        );
+      }
+
+      await fireEvent.change(releaseDateField, {
+        target: { value: "2023-12-19" },
+      });
+
+      await fireEvent.change(scoreField, { target: { value: "4.0" } });
+
+      await userEvent.click(addButton);
+
+      const listPageHeading = await screen.findByRole("heading", {
+        name: listPageHeadingText,
+      });
+
+      expect(listPageHeading).toBeInTheDocument();
     });
   });
 });
