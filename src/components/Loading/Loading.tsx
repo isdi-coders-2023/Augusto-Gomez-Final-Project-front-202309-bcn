cbddc6e020be99d8ea8559d9a27910e5cd2a1421
Augusto Gomez <agomezsantos95@gmail.com>
2023-12-07 19:23:07 +0100

    Remove unnecesary div on Loading component

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,17 +1,15 @@
 import LoadingStyled from "./LoadingStyled";
 
 const Loading = (): React.ReactElement => (
-  <LoadingStyled className="loading">
-    <div className="loading__container">
-      <span className="loading__text">Loading . . .</span>
-      <img
-        src="images/loading.svg"
-        alt="Loading icon"
-        className="loading__icon"
-        width="200"
-        height="200"
-      />
-    </div>
+  <LoadingStyled className="loading__container">
+    <span className="loading__text">Loading . . .</span>
+    <img
+      src="images/loading.svg"
+      alt="Loading icon"
+      className="loading__icon"
+      width="200"
+      height="200"
+    />
   </LoadingStyled>
 );
 
