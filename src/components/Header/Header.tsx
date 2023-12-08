a45025f7f53d8e271de360acce1beb8cc1c85626
Augusto Gomez <agomezsantos95@gmail.com>
2023-12-08 10:24:48 +0100

    Change resolution of images to save resources

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ const Header = (): React.ReactElement => (
   <HeaderStyled className="main-header">
     <img
       className="main-header__logo"
-      src="images/flixpicks_logo.webp"
+      src="images/flixpicks_logo.svg"
       alt="flixpicks logo on black letters"
       width="197"
       height="100"
