8632ad06f8f1b5461ea745102899a265160c69b0
Augusto Gomez <agomezsantos95@gmail.com>
2023-12-10 22:17:40 +0100

    Add happy path testing suite for modifyMovieFromApi function

---
+++ src/hooks/__tests__/modifyMovieFromApi.test.tsx
@@ -0,0 +1,21 @@
+import { renderHook } from "@testing-library/react";
+import useMoviesApi from "../useMoviesApi";
+import { providerWrapper } from "../../testUtils/testUtils";
+import movieMock from "../../mocks/movieMock";
+import { modifiedMoviesMock } from "../../mocks/moviesMocks";
+
+describe("Given a modifyMovieFromApi function on a useMoviesApi", () => {
+  describe("When it receives an id for the movie Arrival", () => {
+    test("Then it should return the movie Arrival modified", async () => {
+      const {
+        result: {
+          current: { modifyMovieFromApi },
+        },
+      } = renderHook(() => useMoviesApi(), { wrapper: providerWrapper });
+
+      const movie = await modifyMovieFromApi(movieMock._id);
+
+      expect(movie).toStrictEqual(modifiedMoviesMock[0]);
+    });
+  });
+});
