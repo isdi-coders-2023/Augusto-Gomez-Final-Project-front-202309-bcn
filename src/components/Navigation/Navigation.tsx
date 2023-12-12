e7188c05025463a299e2c8639744227dfd5c1b0e
Augusto Gomez <agomezsantos95@gmail.com>
2023-12-12 19:06:05 +0100

    Add testing suite for modifyMovieFromApy sad path

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -8,7 +8,7 @@ const Navigation = (): React.ReactElement => (
         <NavLink className="navigation-bar__link" to="/home">
           <span className="navigation-bar__text">Home</span>
           <img
-            src="images/homeIcon.svg"
+            src="/images/homeIcon.svg"
             alt="House navigation icon"
             width="48"
             height="48"
@@ -20,7 +20,7 @@ const Navigation = (): React.ReactElement => (
         <NavLink className="navigation-bar__link" to="/add-movie">
           <span className="navigation-bar__text">Add movie</span>
           <img
-            src="images/addIcon.svg"
+            src="/images/addIcon.svg"
             alt="Add movie navigation icon"
             width="48"
             height="48"
--- src/hooks/__tests__/modifyMovieFromApi.test.tsx
+++ src/hooks/__tests__/modifyMovieFromApi.test.tsx
@@ -3,6 +3,10 @@ import useMoviesApi from "../useMoviesApi";
 import { providerWrapper } from "../../testUtils/testUtils";
 import movieMock from "../../mocks/movieMock";
 import { modifiedMoviesMock } from "../../mocks/moviesMocks";
+import { server } from "../../mocks/msw/node";
+import { errorHandlers } from "../../mocks/msw/errorHandlers";
+import { toast } from "react-toastify";
+import { setStyle } from "../../utils/toastifyFunctions";
 
 describe("Given a modifyMovieFromApi function on a useMoviesApi", () => {
   describe("When it receives an id for the movie Arrival", () => {
@@ -16,6 +20,26 @@ describe("Given a modifyMovieFromApi function on a useMoviesApi", () => {
       const movie = await modifyMovieFromApi(movieMock, movieMock._id);
 
       expect(movie).toStrictEqual(modifiedMoviesMock[0]);
+    });
+  });
+
+  describe("When it receives an id for the movie Arrival and the response from the API fails", () => {
+    test("Then it should call the toast function with a 'Error! Failed to modify a movie' message", async () => {
+      server.use(...errorHandlers);
+      toast.error = vi.fn();
+
+      const {
+        result: {
+          current: { modifyMovieFromApi },
+        },
+      } = renderHook(() => useMoviesApi(), { wrapper: providerWrapper });
+
+      await modifyMovieFromApi(movieMock, movieMock._id);
+
+      expect(toast.error).toHaveBeenCalledWith(
+        "Error! Failed to modify a movie",
+        setStyle("#d65745", "#F3CDC8"),
+      );
     });
   });
 });
